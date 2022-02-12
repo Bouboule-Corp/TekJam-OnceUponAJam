e2ea788aba274549d2bc7574a691698c21a4ff46
Arthur <arthur.boucard@hotmail.fr>
2022-02-12 18:57:16 +0100

    added card component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div id="card1">
+      <AnimatedCard
+        title='green.jpg'
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AnimatedCard from './components/AnimatedCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AnimatedCard
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  #card1 {
+    margin: 0vw 30vw;
+  }
 </style>
---
+++ src/components/AnimatedCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card">
+        <div>
+            <!-- <img v-bind:src="'../assets/' + {img}" /> -->
+            <img :src="resolve_img_url(img)">
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+        show: false,
+    }),
+    props: {
+        img: String
+    },
+    methods: {
+        resolve_img_url: function (path) {
+            let images = require.context('../assets', false, /\.png$|\.jpg$/)
+            return images("./"+path)
+        }
+    }
+  }
+</script>
+
+<style>
+    .card {
+        background-color: black;
+        border-radius: 20px;
+        width: 40vw;
+        height: 35vh;
+    }
+</style>
