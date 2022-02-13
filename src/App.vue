6984b8f2a5d4730f74f8c8530077870ee8406f0f
Arthur <63443236+ArthurBoucard@users.noreply.github.com>
2022-02-13 21:57:24 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,50 @@
 <template>
   <div id="app">
     <Home/>
+    <img id="bg" src="./assets/arrow_bg.png">
+    <div id="card1">
+      <AnimatedCard>
+        <img src="./assets/test.webp">
+      </AnimatedCard>
+    </div>
   </div>
 </template>
 
 <script>
 import Home from './Home.vue'
+import AnimatedCard from './components/AnimatedCard.vue'
 
 export default {
   name: 'App',
   components: {
-    Home
+    Home,
+    AnimatedCard
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Roboto;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 5px;
-}
+  #app {
+    font-family: Roboto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 5px;
+  }
+  #bg {
+    position: absolute;
+  }
+  #bg_bottom {
+    position: absolute;
+    background-color: red;
+    bottom: 0;
+    /* height: 40vh;
+    width: 100vw; */
+  }
+  #card1 {
+    position: relative;
+    top: 90vw;
+    margin: 0vw 30vw;
+  }
 </style>
